cce4260ffdf4a1ff2034f230ae115e040db71c3d
sveinjoe <sveinjoe@gmail.com>
2019-04-30 20:53:14 +0800

    这是一个在网页有滚动条时自动在网页底部添加一个scrollup的按钮 <!--图片按钮样式添加这段代码--> <link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/image.css"> <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script> <script src="/api/scrollup/js/jquery.scrollUp.min.js"></script> </head> <script> 	$(function () { 				$.scrollUp({ 					animation: 'fade', 					activeOverlay: '#00FFFF', 					scrollImg: { active: true, type: 'background', src: 'img/top.png' } 				}); 			}); </script>
    <!--TAB样式添加这段代码-->
    <link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/tab.css">
    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
    <script src="/api/scrollup/js/jquery.scrollUp.min.js"></script>
    </head>
    <script>
    	$(function () {
    				$.scrollUp({
    					animation: 'slide',
    					activeOverlay: '#00FFFF'
    				});
    			});
    </script>
    
    <!--pill样式添加这段代码-->
    <link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/pill.css">
    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
    <script src="/api/scrollup/js/jquery.scrollUp.min.js"></script>
    </head>
    <script>
    	$(function () {
    				$.scrollUp({
    					animation: 'fade',
    					activeOverlay: '#00FFFF'
    				});
    			});
    </script>
    
    <!--link样式添加这段代码-->
    <link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/link.css">
    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
    <script src="/api/scrollup/js/jquery.scrollUp.min.js"></script>
    </head>
    <script>
    	$(function () {
    				$.scrollUp({
    					animation: 'fade',
    					activeOverlay: '#00FFFF'
    				});
    			});
    </script>

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ScrollUp demo - Mark Goodyear</title>
+<link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/image.css">
+<link id="scrollUpTheme" rel="stylesheet" href="/api/scrollup/css/labs.css?1.1">
+<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+<script src="/api/scrollup/js/jquery.scrollUp.min.js"></script>
+</head>
+<script>
+	$(function () {
+				$.scrollUp({
+					animation: 'fade',
+					activeOverlay: '#00FFFF',
+					scrollImg: { active: true, type: 'background', src: 'img/top.png' }
+				});
+			});
+</script>
+<body>
+<style>
+		#carbonads-container {
+			position: fixed;
+			top: 80px;
+			right: 20px;
+		}
+		.carbonad {
+			background: rgba(255,255,255, .7);
+			border: 1px solid #eee;
+		}
+		</style>
+<div id="carbonads-container"><div class="carbonad"><div id="azcarbon"></div><script type="text/javascript">var z = document.createElement("script"); z.type = "text/javascript"; z.async = true; z.src = "https://engine.carbonads.com/z/34234/azcarbon_2_1_0_VERT"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(z, s);</script></div></div>
+<div class="header">
+<a href="//markgoodyear.com" class="logo">Mark Goodyear</a>
+<a href="//markgoodyear.com/2013/01/scrollup-jquery-plugin" class="view-article">View article</a>
+</div>
+<div class="container">
+<h1>
+scrollUp 2.0.0 Demo
+</h1>
+<p>See the <a href="https://github.com/markgoodyear/scrollup/blob/master/CHANGELOG.md" target="_blank">scrollUp v2.0.0 changelog</a> for more information.</p>
+<h2>Demo styles</h2>
+<ul class="themes">
+<li class="first"><a href="#" class="tab-switch">Tab</a></li>
+<li><a href="#" class="pill-switch">Pill</a></li>
+<li><a href="#" class="link-switch">Link</a></li>
+<li><a href="#" class="image-switch">Image</a></li>
+</ul>
+<em class="light">Demo styles can be found in the <code>css/themes/</code> folder.</em>
+<h2>activeOverlay Demo</h2>
+<p>For demo purposes, toggle the activeOverlay below.</p>
+<a id="toggleActive">Show activeOverlay</a>
+<div style="clear:both"></div>
+<h2>Dummy content</h2>
+<p><strong>Dummy content to ensure the page is long enough to scroll on all screen sizes.</strong></p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+</div>
+</body>
+</html>
